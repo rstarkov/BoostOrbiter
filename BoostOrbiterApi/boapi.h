75c2ce39fd7a783b85064f2c96b2b09874dc5304
rstarkov <rstarkov@localhost>
2010-01-02 19:53:27 +0000

    Basic visual studio project setup and directory structure.

---
+++ BoostOrbiterApi/boapi.h
@@ -0,0 +1,6 @@
+#ifndef __BOOSTORBITER_BOAPI_H__
+#define __BOOSTORBITER_BOAPI_H__
+
+OBJHANDLE boapiGetVesselByName(const std::string& name);
+
+#endif // __BOOSTORBITER_BOAPI_H__
---
+++ PrecompiledBoostOrbiter.h
@@ -0,0 +1,30 @@
+#ifndef __BOOSTORBITER_PRECOMPILEDBOOSTORBITER_H__
+#define __BOOSTORBITER_PRECOMPILEDBOOSTORBITER_H__
+
+#define WIN32_LEAN_AND_MEAN
+#define NOSERVICE
+#define NOMCX
+#define NOIME
+#define NOSOUND
+#define NOKANJI
+#define NOIMAGE
+#define NOTAPE
+
+#define STRICT
+
+#include <windows.h>
+#include <stdio.h>
+#include <math.h>
+#include <malloc.h>
+
+#include <Orbiterapi.h>
+#include <Orbitersdk.h>
+
+#include <map>
+#include <vector>
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+#endif // __BOOSTORBITER_PRECOMPILEDBOOSTORBITER_H__

79ca9a673453a284e233adea8a862edc50b42225
Roman Starkov <rstarkov@users.noreply.github.com>
2010-01-03 17:33:54 +0000

    Directory structure / namespace tidy up.

---
+++ borb/boapi.h
@@ -0,0 +1,12 @@
+#ifndef __BOOSTORBITER_BORB_BOAPI_H__
+#define __BOOSTORBITER_BORB_BOAPI_H__
+
+namespace borb
+{
+    namespace boapi
+    {
+        OBJHANDLE GetVesselByName(const std::string& name);
+    }
+}
+
+#endif // __BOOSTORBITER_BORB_BOAPI_H__

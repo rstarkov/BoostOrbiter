83ba8050d89372736aa18d4f6c012f564077ace7
Roman Starkov <rstarkov@users.noreply.github.com>
2010-01-03 23:33:03 +0000

    License stuff.

--- borb/GdiHelpers.cpp
+++ borb/GdiHelpers.cpp
@@ -1,3 +1,9 @@
+//----------------------------------------------------------------------------
+// This file is part of the BoostOrbiter project, and is subject to the terms
+// and conditions defined in file 'license.txt'. Full list of contributors is
+// available in file 'contributors.txt'.
+//----------------------------------------------------------------------------
+
 #include <PrecompiledBoostOrbiter.h>
 #include <SimpleIni/SimpleIni.h>
 #include "GdiHelpers.h"

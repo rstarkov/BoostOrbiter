dbb8e9c7c35b6dc11945ad7fc0c02069afeaaa43
rstarkov <rstarkov@localhost>
2010-01-03 23:33:03 +0000

    License stuff.

--- PrecompiledBoostOrbiter.h
+++ PrecompiledBoostOrbiter.h
@@ -1,3 +1,9 @@
+//----------------------------------------------------------------------------
+// This file is part of the BoostOrbiter project, and is subject to the terms
+// and conditions defined in file 'license.txt'. Full list of contributors is
+// available in file 'contributors.txt'.
+//----------------------------------------------------------------------------
+
 #ifndef __BOOSTORBITER_PRECOMPILEDBOOSTORBITER_H__
 #define __BOOSTORBITER_PRECOMPILEDBOOSTORBITER_H__
 
